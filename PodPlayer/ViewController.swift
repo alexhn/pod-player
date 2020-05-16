73319ea8b5887dcd6df10b1c514ddcb88e2b2c09
Aleksandr Nikiforov <alex@alexn.ee>
2020-05-16 13:38:39 +0300

    Initial Commit

---
+++ PodPlayer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  PodPlayer
+//
+//  Created by Aleksandr Nikiforov on 16.05.2020.
+//  Copyright © 2020 Aleksandr Nikiforov. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
